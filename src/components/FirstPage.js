54309dbd5a596c2ce9409faea810b11f6ac76ae3
wangqi <1424195010@qq.com>
2016-05-14 17:42:58 +0800

    musictool

--- src/components/FirstPage.js
+++ src/components/FirstPage.js
@@ -1,13 +1,17 @@
-require('styles/FirstPage.css');
-
 import React from 'react';
 
 class FirstPage extends React.Component {
   render() {
     return (
-      <div className="FirstPage">
-        第一页
-      </div>
+      <div className="fp-section wqfirstpage">
+        <div className="fp-slide-wrap">
+        	<div className="fp-slide">1</div>
+        	<div className="fp-slide">2</div>
+        	<div className="fp-slide">3</div>
+        	<div className="fp-slide">4</div>
+        	<div className="fp-slide">5</div>
+        </div>
+    </div>
     );
   }
 }
--- src/components/Main.js
+++ src/components/Main.js
@@ -3,24 +3,44 @@ require('styles/App.css');
 require('styles/fullpage.css');
 
 import React from 'react';
-// import FirstPage from 'components/FirstPage';
+import FirstPage from 'components/FirstPage';
+import MusicTool from 'components/MusicTool';
 
 class AppComponent extends React.Component {
+  constructor(props) {
+      super(props);
+      this.state = {
+        planeDisplayed:true,
+        musicurl:'../images/1.mp3'
+      };
+  }
+  Mtoggle(e){
+    this.setState({
+      planeDisplayed: !this.state.planeDisplayed,
+    });
+    let bgsound=new Audio(this.state.musicurl);
+    let M=document.getElementById("M");
+    let flag=this.state.planeDisplayed;
+    console.log(flag);
+    if(flag){
+      M.play();
+      //bgsound.loop = true;
+    }else{
+      M.pause();
+    }
+
+  }
   render() {
     return (
-	    <div id="sectionContent" className="fp-section-content">
-        <div className="fp-section">
-            <div className="fp-slide-wrap">
-              <div className="fp-slide">1</div>
-              <div className="fp-slide">2</div>
-              <div className="fp-slide">3</div>
-              <div className="fp-slide">4</div>
-              <div className="fp-slide">5</div>
-            </div>
-          </div>
-        <div className="fp-section">2</div>
-        <div className="fp-section">3</div>
-    </div>
+      <div>
+        <MusicTool  Mtoggle={this.Mtoggle}
+                    planeDisplayed={this.state.planeDisplayed }/>
+  	    <div id="sectionContent" className="fp-section-content">
+          <FirstPage />
+          <div className="fp-section">2</div>
+          <div className="fp-section">3</div>
+        </div>
+      </div>  
     );
   }
 }
